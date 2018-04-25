de938b175d9f6594ba14e4e89d9da7455e24fd62
Erwan BERNARD <bernard.erwan@gmail.com>
2018-04-25 11:04:33 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Simple Cli for `gh-badges` library
+
+I find that gh-badges built in command line tool have strange behaviour. So I rebuild a simple command line tool on top of `gh-badges` library.
+
+## Installation
+
+```bash
+yarn
+yarn link
+```
+
+## Usage:
+
+```
+badges-cli <first> <second> <colorscheme>
+
+Print SVG image for the badge
+
+Positionals:
+  first        first text                                               [string]
+  second       second text                                              [string]
+  colorscheme  color
+   [string] [choices: "brightgreen", "green", "yellow", "yellowgreen", "orange",
+                        "red", "blue", "grey", "gray", "lightgrey", "lightgray"]
+
+Options:
+  --version   Show version number                                      [boolean]
+  -h, --help  Show help                                                [boolean]
+
+Examples:
+  badges-cli build passed green > mybadge.svg
+  badges-cli build failed red > mybadge.svg
+```
--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 
 const badge = require('gh-badges')
 var argv = require('yargs')
-    .command('$0 <first> <second> <colorscheme>', 'start the server', (yargs) => {
-        yargs
-            .positional("first", {describe: 'first text', type: 'string', required: true})
-            .positional("second", {describe: 'second text', type: 'string', required: true})
-            .positional("colorscheme", {describe: 'left color', type: 'string', required: true})
-            .choices("colorscheme", ["brightgreen", "green", "yellow", "yellowgreen", "orange", "red", "blue", "grey", "gray", "lightgrey", "lightgray"])
-    })
-    .alias("h", "help")
-    .version("0.0.1")
-    .argv;
+  .command('$0 <first> <second> <colorscheme>', 'Print SVG image for the badge', (yargs) => {
+    yargs
+    .positional("first", {describe: 'first text', type: 'string', required: true})
+    .positional("second", {describe: 'second text', type: 'string', required: true})
+    .positional("colorscheme", {describe: 'color', type: 'string', required: true})
+    .choices("colorscheme", ["brightgreen", "green", "yellow", "yellowgreen", "orange", "red", "blue", "grey", "gray", "lightgrey", "lightgray"])
+    .example("$0 build passed green > mybadge.svg")
+    .example("$0 build failed red > mybadge.svg")
+  })
+  .alias("h", "help")
+  .version("0.0.1")
+  .argv;
 
 // Optional step, to have accurate text width computation.
 const format = {
